12437aeaf0d9bbc6de636cc36ebef705e40b3cb5
CherniakYura <juracherniak@gmail.com>
2024-02-02 22:48:44 +0100

    Implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length - 1;
+
+    while (low < high) {
+        const center = Math.round((low + high) / 2);
+        if (haystack[center] === needle) { return true }
+        if (haystack[low] === needle) { return true }
+        if (haystack[high] === needle) { return true }
+        else if (haystack[center] < needle) {
+            low = center + 1;
+        }
+        else if (haystack[center] > needle) {
+            high = center - 1;
+        }
+    }
+
+    return false
 }
 
