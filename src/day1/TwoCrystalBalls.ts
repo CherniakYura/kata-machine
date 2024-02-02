fbf0e1294d038e6a1c50f75abd407bdf3daf99df
CherniakYura <juracherniak@gmail.com>
2024-02-02 22:52:27 +0100

    implement two crystal balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    let index = 0;
+    while (index < breaks.length) {
+        if (breaks[index]) {
+            index -= Math.sqrt(breaks.length);
+            while (!breaks[index]) {
+                index++;
+            }
+            return index;
+        }
+        index += Math.sqrt(breaks.length);
+    }
+    return -1
 }
