653f05470a3d55d346ff4a1388be45e3e8324c76
CherniakYura <juracherniak@gmail.com>
2024-02-04 15:50:16 +0100

    CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,17 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+import in_order_search from "./BTInOrder";
 
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a && b) {
+        const a_nums = in_order_search(a);
+        const b_nums = in_order_search(b);
+
+        for (let i = 0; i < a_nums.length; i++) {
+            if (a_nums[i] !== b_nums[i]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    return false
 }
