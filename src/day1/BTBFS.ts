ebb1d3e0ac397d720ed01c16006ff816c278ddab
CherniakYura <juracherniak@gmail.com>
2024-02-04 14:11:11 +0100

    BFS cучка

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = new Queue<BinaryNode<number>>()
+    queue.enqueue(head)
+
+    while (queue.length) {
+        const node = queue.deque()
+
+        if (node?.value === needle) {
+            return true
+        }
+
+        if (node?.left) {
+            queue.enqueue(node.left)
+        }
+        if (node?.right) {
+            queue.enqueue(node.right)
+        }
+    }
+    return false
 }
