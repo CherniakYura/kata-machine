8ca5435bb691bde7f4cc3eda660d4417d22b05a4
CherniakYura <juracherniak@gmail.com>
2024-02-04 15:50:23 +0100

    DFS on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,18 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, needle: number): boolean {
+    if (node === null) {
+        return false
+    }
+    if (node.value === needle) {
+        return true
+    }
 
+    if (needle > node.value) {
+        return search(node.right, needle)
+    }
+    else {
+        return search(node.left, needle)
+    }
+}
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle)
 }
