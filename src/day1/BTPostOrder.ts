0fd800b34d06aba75041ce459238339f9f601a8f
CherniakYura <juracherniak@gmail.com>
2024-02-04 13:35:40 +0100

    Other traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,15 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    const result: number[] = []
+    traverse(head, result)
+    return result
+}
 
+function traverse(node: BinaryNode<number> | null, result: number[]) {
+    if (node === null) {
+        return
+    }
+
+    traverse(node.left, result)
+    traverse(node.right, result)
+    result.push(node.value)
 }
