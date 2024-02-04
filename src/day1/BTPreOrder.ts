b4e27c20cd5e3c6c0784d59c862102a2a879748a
CherniakYura <juracherniak@gmail.com>
2024-02-04 13:33:08 +0100

    PreOrder first try

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,14 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const result: number[] = []
+    traverse(head, result)
+    return result
+}
+function traverse(node: BinaryNode<number> | null, result: number[]) {
+    if (node === null) {
+        return
+    }
 
+    result.push(node.value)
+    traverse(node.left, result)
+    traverse(node.right, result)
 }
