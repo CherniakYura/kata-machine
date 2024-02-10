e74ff2a21ed80cfa02ced80cb819458fafd5827f
CherniakYura <juracherniak@gmail.com>
2024-02-10 15:34:33 +0100

    Implement BFS on graph matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,44 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const queue = new Queue<number>();
+    const visited = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+    const path: number[] = [];
+
+    queue.enqueue(source)
+    visited[source] = true
+
+    while (queue.length > 0) {
+        const node = queue.deque() as number
+
+        if (node === needle) {
+            break
+        }
+
+        graph[node].forEach((w, i) => {
+            if (visited[i]) {
+                return
+            }
+            if (w > 0) {
+                prev[i] = node
+                visited[i] = true
+                queue.enqueue(i)
+            }
+        })
+    }
+
+    if (prev[needle] === -1) {
+        return null
+    }
+
+    let current = needle
+    let out = []
+
+    while (current !== -1) {
+        out.push(current)
+        current = prev[current]
+    }
+
+    return out.reverse()
 }
